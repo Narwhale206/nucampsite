9b630f9146149ee58c957829ae01ffb9978f7d42
Belugaman <narwhale360y@gmail.com>
2022-07-16 14:37:12 -0700

    test code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import NucampLogo from './app/assets/img/logo.png';
+import './App.css';
+import CampsitesList from './features/campsites/CampsitesList';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <CampsitesList />
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import { Container, Row, Col } from 'reactstrap'; 
+const Footer = () => {
+    return (
+        <footer className='site-footer'>
+            <container> <h5>Social</h5>
+                        <a
+                            className='btn btn-social-icon btn-instagram'
+                            href='http://instagram.com/'
+                        >
+                            <i className='fa fa-instagram' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-facebook'
+                            href='http://www.facebook.com/'
+                        >
+                            <i className='fa fa-facebook' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-twitter'
+                            href='http://twitter.com/'
+                        >
+                            <i className='fa fa-twitter' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-google'
+                            href='http://youtube.com/'
+                        >
+                            <i className='fa fa-youtube' />
+                        </a>
+
+            </container>
+        </footer>
+    )
+}
+
+
+
+import Footer from './components/Footer';
