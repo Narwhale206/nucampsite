854946e973b82175613116b46b95d08899d06738
Belugaman <narwhale360y@gmail.com>
2022-07-17 13:13:45 -0700

    new code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import CampsitesList from './features/campsites/CampsitesList';
-
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <CampsitesList />
-      <Footer />
-      
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <CampsitesList />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
+
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,12 @@ import NucampLogo from '../app/assets/img/logo.png';
 
 const Header = () => {
     return (
-<Navbar dark color='primary' sticky='top' expand='md'>
-    <NavbarBrand href='/'>
-        <img src={Nucamplogo} alt='nucamp logo' />
-    </NavbarBrand>
-</Navbar>
+        <Navbar dark color='primary' sticky='top' expand='md'>
+            <NavbarBrand href='/'>
+                <img src={NucampLogo} alt='nucamp logo' />
+            </NavbarBrand>
+        </Navbar>
     );
-
-    };
+};
 
 export default Header;
